397088d9c8447d8ab88a8ffa7d1b6e35747a22c2
BillFu <Bill.Fu@hotmail.com>
2022-09-17 11:20:52 +0800

    out 8 LM data set.

---
+++ run_export_ISEMECO.sh
@@ -0,0 +1,5 @@
+bin/LMExporter lm_export_conf_isemeco1.json
+
+echo "--------------*************************************------------------"
+
+bin/LMExporter lm_export_conf_isemeco2.json
---
+++ run_export_Lu_White.sh
@@ -0,0 +1,5 @@
+bin/LMExporter lm_export_conf_white1.json
+
+echo "--------------*************************************------------------"
+
+bin/LMExporter lm_export_conf_lu.json
