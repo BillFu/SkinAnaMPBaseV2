b7e4e5b7bf3fec0ac56f7ee402b04720757240f7
BillFu <Bill.Fu@hotmail.com>
2022-10-06 16:15:14 +0800

    www

--- SkinAnaMPBase/FaceLmExtractV2.cpp
+++ SkinAnaMPBase/FaceLmExtractV2.cpp
@@ -107,8 +107,8 @@ bool LoadFaceMeshModel(const char* faceMeshModelFile, string& errorMsg)
 Mat PaddingImage(const Mat& srcImage, const Rect& bbox,
              int& padLeft, int& padTop)
 {
-    float alphaHor = 0.25;
-    float alphaVer = 0.15;
+    float alphaHor = 0.25; //0.25
+    float alphaVer = 0.45;  //0.15
     Mat croppedImg = srcImage(bbox); // 事先将faceBBox的长宽调整为偶数???
     padLeft = (int)(bbox.width * alphaHor); // both left and right extend padW outside
     padTop = (int)(bbox.height * alphaVer); // both top and bottom extend padH outside
@@ -139,6 +139,8 @@ bool ExtractFaceLm(const Mat& srcImage,
     int padLeft, padTop;
     Mat paddedImg = PaddingImage(srcImage, segResult.faceBBox,
                            padLeft, padTop);
+    
+    imwrite("padR3.png", paddedImg);
     cv::Size padImgSize = paddedImg.size();
     
     //-------------------------*****enter inference*****---------------------------------------------------------
