be3f1155d1332ac368baa00ff72afc7ff2609b7c
BillFu <Bill.Fu@hotmail.com>
2022-10-12 23:08:03 +0800

    www

--- SkinAnaMPBase/Mask/EyebrowMaskV4.cpp
+++ SkinAnaMPBase/Mask/EyebrowMaskV4.cpp
@@ -198,32 +198,12 @@ PtInPolarSeq MakePolarSeqInCCWise(const PtInPolarSeq& oriSeq)
     }
 }
 
-// build a new version of polar point sequence that with even intervals of theta.
-void BuildEvenPolarSeq(const PolarContour& rawPolarSeq,
-                       int num_interval, // how many intervals from 0 to 2*Pi
-                       PolarContour& evenPolarSeq)
+// make the pt with angle near zero placed at the header of seq
+void ReorderPolarPtSeq(const PtInPolarSeq& ccSeq,
+                     PtInPolarSeq& zeroThetaFirstSeq)
 {
-    assert(rawPolarSeq.ptSeq.size() > 2);
+    int numPts = (int)(ccSeq.size());
     
-    int numRawPts = (int)(rawPolarSeq.ptSeq.size());
-    
-    //firstly, add 2*Pi to those nagative thetas, make all the angles fall into [0 2*Pi]
-    PtInPolarSeq posiThetaPtSeq;
-    for(PtInPolarCd ptPC: rawPolarSeq.ptSeq)
-    {
-        if(ptPC.theta < 0)
-            posiThetaPtSeq.push_back(PtInPolarCd(ptPC.r, ptPC.theta + 2*M_PI));
-        else
-            posiThetaPtSeq.push_back(ptPC);
-    }
-    
-    // secondly rebuild the seq to make points allocated in counter clock-wise
-    PtInPolarSeq ccSeq = MakePolarSeqInCCWise(posiThetaPtSeq);
-    
-    // thridly we need to look for the point where 2*PI turns into zero
-    // Here Monotonicity is assumed to be hold, i.e., when in the scaning, theta
-    // would increase continuously, or decrease continuously.
-
     int curID = 0;
     double Th_2PI = 1.8 * M_PI;
     while(curID <= ccSeq.size() - 1)
@@ -232,7 +212,7 @@ void BuildEvenPolarSeq(const PolarContour& rawPolarSeq,
         PtInPolarCd curPt;
         if(curID == 0)
         {
-            prevPt = ccSeq[numRawPts - 1];
+            prevPt = ccSeq[numPts - 1];
         }
         else
             prevPt = ccSeq[curID-1];
@@ -255,22 +235,121 @@ void BuildEvenPolarSeq(const PolarContour& rawPolarSeq,
     // fourthly reorder the seq make the pt with angle near 0 (and positive)
     // locates at the header of sequence
     // the Pt associated with curID should be placed firstly
-    PtInPolarSeq seqStartZeroTheta;
     while(curID <= ccSeq.size() - 1)
     {
-        seqStartZeroTheta.push_back(ccSeq[curID]);
+        zeroThetaFirstSeq.push_back(ccSeq[curID]);
         curID++;
     }
     
     curID = 0;
     while(curID < zeroThetaID)
     {
-        seqStartZeroTheta.push_back(ccSeq[curID]);
+        zeroThetaFirstSeq.push_back(ccSeq[curID]);
         curID++;
     }
+}
+
+// find the initial appropriate values for uID and lID
+void InitIpSetting(const PtInPolarSeq& seq, double angle, int& uID, int& lID)
+{
+    int numPts = (int)(seq.size());
+
+    lID = numPts - 1;
+    uID = 0;
     
+    double uAngle = seq[uID].theta;
+    //double lAngle = seq[lID].theta - 2*M_PI;
+    
+    while(uAngle < angle && uID < numPts -1)
+    {
+        lID = uID;
+        uID++;
+        uAngle = seq[uID].theta;
+    }
+}
+
+// Ip: abbr. of Interpolate
+void IpPolarCd(const PtInPolarCd& uPt, const PtInPolarCd& lPt,
+                        double angle, PtInPolarCd& outPt)
+{
+    double uPt_theta = uPt.theta;
+    double lPt_theta = lPt.theta;
+    
+    if(lPt_theta > uPt_theta)
+        lPt_theta -= 2*M_PI;
+        
+    double t = (angle - lPt_theta) / (uPt_theta - lPt_theta);
+    double r = t * lPt.r + (1.0 - t)* uPt.r;
+    
+    outPt.theta = angle;
+    outPt.r = r;
+}
+
+void IpPolarPtSeqEvenly(const PtInPolarSeq& ascThetaSeq, int numInterval,
+                        const Point& oriPt,
+                        PolarContour& evenPolarSeq)
+{
+    int uID, lID;
+    double angle = 0.0;
+    double dAngle = 2.0*M_PI / numInterval;
+    InitIpSetting(ascThetaSeq, angle, uID, lID); //Ip: interpolate
+    
+    evenPolarSeq.oriPt = oriPt;
+    while(angle < 2*M_PI)
+    {
+        PtInPolarCd uPt = ascThetaSeq[uID];
+        while(angle < uPt.theta)
+        {
+            angle += dAngle;
+            PtInPolarCd outPt;
+            IpPolarCd(ascThetaSeq[uID], ascThetaSeq[lID],
+                                angle, outPt);
+            evenPolarSeq.ptSeq.push_back(outPt);
+        }
+        
+        lID = uID;
+        uID++;
+    }
+}
+
+// build a new version of polar point sequence that with even intervals of theta.
+void BuildEvenPolarSeq(const PolarContour& rawPolarSeq,
+                       int numInterval, // how many intervals from 0 to 2*Pi
+                       PolarContour& evenPolarSeq)
+{
+    assert(rawPolarSeq.ptSeq.size() > 2);
+    
+    int numRawPts = (int)(rawPolarSeq.ptSeq.size());
+    
+    //firstly, add 2*Pi to those nagative thetas, make all the angles fall into [0 2*Pi]
+    PtInPolarSeq posiThetaPtSeq;
+    for(PtInPolarCd ptPC: rawPolarSeq.ptSeq)
+    {
+        if(ptPC.theta < 0)
+            posiThetaPtSeq.push_back(PtInPolarCd(ptPC.r, ptPC.theta + 2*M_PI));
+        else
+            posiThetaPtSeq.push_back(ptPC);
+    }
+    
+    // secondly rebuild the seq to make points allocated in counter clock-wise
+    PtInPolarSeq ccwSeq = MakePolarSeqInCCWise(posiThetaPtSeq);
+    
+    // thridly we need to look for the point where 2*PI turns into zero
+    // Here Monotonicity is assumed to be hold, i.e., when in the scaning, theta
+    // would increase continuously, or decrease continuously.
+    PtInPolarSeq ascThetaSeq; // the seq with ascending thetas
+    ReorderPolarPtSeq(ccwSeq, ascThetaSeq);
+
     // finally, the very important step: interpolate with even angular interval
     // and a step full of challenge
+    IpPolarPtSeqEvenly(ascThetaSeq, numInterval,
+                       rawPolarSeq.oriPt, evenPolarSeq);
+}
+
+void smoothPolarPtSeq(const PolarContour& evenPolarSeq,
+                      PolarContour& smoothPolarSeq)
+{
+    // 5-points moving and smoothing
     
 }
 
@@ -296,8 +375,11 @@ void ForgeEyePgBySegRst(Size srcImgS, const SegMask& eyeSegMask,
     
     PolarContour evenPolarSeq;
     BuildEvenPolarSeq(rawPolarSeq,
-                      72, // how many intervals from 0 to 2*Pi
+                      144, // how many intervals from 0 to 2*Pi
                       evenPolarSeq);
+    
+    // then smoothing the evenly interpolated polar pt seq.
+    
 }
 
 void ForgeEyesMask(const FaceInfo& faceInfo,
