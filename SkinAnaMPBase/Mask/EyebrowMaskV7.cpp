f4646c4fb658d5cc58b8cfd3a8d301a2123da463
BillFu <Bill.Fu@hotmail.com>
2022-10-27 20:40:56 +0800

    www

--- SkinAnaMPBase/Mask/EyebrowMaskV7.cpp
+++ SkinAnaMPBase/Mask/EyebrowMaskV7.cpp
@@ -160,7 +160,7 @@ void FixERPsBySegEyeCP(const Point2i eyeRefPts[NUM_PT_EYE_REFINE_GROUP], // inpu
 /**********************************************************************************************/
 
 void ForgeInitEyePg(const Point2i eyeRefinePts[NUM_PT_EYE_REFINE_GROUP],
-                    float expandScale, POLYGON& initEyePg)
+                    POLYGON& initEyePg)
 {
     POLYGON refinedPolygon;
         
@@ -176,20 +176,6 @@ void ForgeInitEyePg(const Point2i eyeRefinePts[NUM_PT_EYE_REFINE_GROUP],
         int index = ptIDsOnCurve[i];
         initEyePg.push_back(eyeRefinePts[index]);
     }
-    
-    /*
-    CONTOUR refineCurve;
-    DenseSmoothPolygon(coarseCurve, 200, refineCurve, true);
-    
-    Rect coaPgBBox = boundingRect(refineCurve); // maybe need padding and enlarge
-    Point2i centerPt = (coaPgBBox.tl() + coaPgBBox.br())/ 2;
-    
-    for(Point2i pt: refineCurve)
-    {
-        Point2i expPt = (pt - centerPt) * expandScale + centerPt;
-        initEyePg.push_back(expPt);
-    }
-    */
 }
 
 // 用分割的结果构造出一只眼睛的轮廓多边形
@@ -202,30 +188,27 @@ void ForgeEyePgBySnakeAlg(Size srcImgS,
                           POLYGON& eyePg)
 {
     POLYGON initEyePg;
-    ForgeInitEyePg(eyeRefinePts, 1.4, initEyePg);
+    ForgeInitEyePg(eyeRefinePts, initEyePg);
     
     Rect initEyePgBBox = boundingRect(initEyePg); // maybe need padding and enlarge
-
     Rect eyeMaskBBox = RectNOS2RectSS(srcImgS, eyeSegMask.bbox);
     
     Mat segEyeMaskSS;
     resize(eyeSegMask.mask, segEyeMaskSS, eyeMaskBBox.size(), INTER_NEAREST);
     
-    if(RectContainsRect(initEyePgBBox, eyeMaskBBox) == false)
-    {
-        // enlarge the initEyePgBBox by use of union operation
-        initEyePgBBox = initEyePgBBox | eyeMaskBBox;
-    }
-    
+    // enlarge the initEyePgBBox by use of union operation
+    initEyePgBBox = initEyePgBBox | eyeMaskBBox;
+
     // make initEyePgBBox enlarged
     int padSize = 50;
     InflateRect(padSize, initEyePgBBox);
 
     Mat workImg(initEyePgBBox.size(), CV_8UC1, Scalar(0));
     Rect relativeRect = CalcRelativeRect(initEyePgBBox, eyeMaskBBox);
-    segEyeMaskSS.copyTo(workImg(relativeRect));
-    imwrite("InitSegMask.png", workImg);
+    segEyeMaskSS.copyTo(workImg(relativeRect)); // 将segEyeMask先复写到workImg
+    // imwrite("InitSegMask.png", workImg);
 
+    // 坐标转换，由source space转入initEyePgBBox框定的局部坐标系，也称工作坐标系(WC)
     POLYGON initEyePgWC;
     for(Point2i pt: initEyePg)
     {
@@ -233,24 +216,27 @@ void ForgeEyePgBySnakeAlg(Size srcImgS,
         initEyePgWC.push_back(relaPt);
     }
     
+    // --- 在栅格域合并两个Mask
     CONTOURS contours;
     contours.push_back(initEyePgWC);
     // convert init eye polygon to raster and calculate the union of it and eye seg mask.
+    // 再把initEyePgWC转化为栅格形式，并与segEyeMask会师，合并
     drawContours(workImg, contours, 0, Scalar(255), FILLED);
 
     int eroRadius = 20;
     Mat element = getStructuringElement( MORPH_ELLIPSE,
                            Size(2*eroRadius + 1, 2*eroRadius + 1),
                            Point(eroRadius, eroRadius));
-    Mat workImg1;
-    dilate(workImg, workImg1, element);
+    dilate(workImg, workImg, element);
     // imwrite("initDataAC.png", workImg1);
 
+    // --- 把合并后的结果转为矢量域
     CONTOURS contours1;
-    //CHAIN_APPROX_SIMPLE会导致轮廓点稀疏，不紧密相挨，导致后面我们的算法失败
-    findContours(workImg1, contours1,
+    findContours(workImg, contours1,
             cv::noArray(), RETR_EXTERNAL, CHAIN_APPROX_NONE);
-    
+
+    //--------------------------------------------------------------
+    // ---- just for debugging
     Mat canvas(initEyePgBBox.size(), CV_8UC1, Scalar(0));
     segEyeMaskSS.copyTo(canvas(relativeRect));
     drawContours(canvas, contours1, 0, Scalar(150), 2);
